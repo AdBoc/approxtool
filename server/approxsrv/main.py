cfc6a69e8642d30dad18125cf6e073eb2bd8f2ec
AdBoc <adrianboczon98@gmail.com>
2021-06-20 21:21:59 +0200

    implement python service with asyncio

--- server/approxsrv/main.py
+++ server/approxsrv/main.py
@@ -1,11 +1,12 @@
-from concurrent import futures
+import asyncio
 import approximation_pb2
 import approximation_pb2_grpc
 import grpc
+import logging
 
 
 class Servicer(approximation_pb2_grpc.ApproximationServiceServicer):
-    def GetApproximation(self, request, context):
+    async def GetApproximation(self, request, context):
         print(f"Received request: {request.model} {request.type}")
 
         calculation_result = approximation_pb2.CalculationResult()
@@ -13,9 +14,18 @@ class Servicer(approximation_pb2_grpc.ApproximationServiceServicer):
         return calculation_result
 
 
-if __name__ == '__main__':
-    server = grpc.server(futures.ThreadPoolExecutor(3))
+async def serve() -> None:
+    listen_addr = '[::]:9002'
+    server = grpc.aio.server()
     approximation_pb2_grpc.add_ApproximationServiceServicer_to_server(Servicer(), server)
-    server.add_insecure_port("[::]:9002")
-    server.start()
-    server.wait_for_termination()
+    server.add_insecure_port(listen_addr)
+    logging.info("Starting server on %s", listen_addr)
+    await server.start()
+    try:
+        await server.wait_for_termination()
+    except KeyboardInterrupt:
+        await server.stop(0)
+
+
+if __name__ == '__main__':
+    asyncio.run(serve())
